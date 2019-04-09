e7f280e7715f676614f958f5d3039508a3858e24
Jet-Lu <Jet__Lu@163.com>
2019-04-09 14:47:11 +0800

    change

--- business-manage/core-service/overview/web-app/web-app.js
+++ business-manage/core-service/overview/web-app/web-app.js
@@ -18,20 +18,10 @@ var app = new Vue({
         { label: '总览', url: './resource-overview.html', active: true },
         { label: '资源告警', url: '../resource-alarm/resource-alarm.html', active: false }
       ],
-      marginTop_togglePage: ''
     }
   },
   created() { },
-  mounted() {
-    console.log(this.$refs.homePage, this.$refs.main_row);
-    const calcHeight = this.$refs.homePage.clientHeight - this.$refs.main_row.$el.clientHeight - 240;
-    console.log(calcHeight, this.$refs.homePage.clientHeight, this.$refs.main_row.$el.clientHeight);
-    if (calcHeight < 0) {
-      this.marginTop_togglePage = '20px';
-    } else {
-      this.marginTop_togglePage = calcHeight + 'px';
-    }
-  },
+  mounted() { },
   methods: {
     togglePage(evt) {
       window.location.href = evt.url;
