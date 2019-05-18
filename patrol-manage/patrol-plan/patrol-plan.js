3eb1252868c3344d74ce95fe9c9ba515736e185f
Jet-Lu <Jet__Lu@163.com>
2019-05-19 00:31:05 +0800

    解决 添加巡检项 问题

--- patrol-manage/patrol-plan/patrol-plan.js
+++ patrol-manage/patrol-plan/patrol-plan.js
@@ -152,7 +152,7 @@ var app = new Vue({
       type: '22'
     }],
     multipleSelection: [],
-    countNum: 0,
+    countNum: '0',
     addObj: {
       uuic: '',
       name: '',
@@ -172,25 +172,11 @@ var app = new Vue({
     this.setData.len = this.setTableData.length;
   },
   mounted() { },
-  watch: {
-    // addObj: {
-    //   uuid: this.countNum,
-    //   name: this.addObj.name,
-    //   describe: this.addObj.describe,
-    //   reference_min: this.addObj.reference_min,
-    //   reference_max: this.addObj.reference_max,
-    //   unit: this.addObj.unit,
-    //   type: '人工巡检',
-    //   inspect_val: this.addObj.inspect_val,
-    //   detail: this.addObj.detail,
-    // }
-  },
   methods: {
-    pushDataToTable(obj) {
-      this.addDialogTable.push(obj);
-    },
     addInspect() {
-      this.countNum ++;
+      this.addDialogTable.forEach(v => {
+        v.isEdit = false;
+      })
       this.addDialogTable.push({
         uuid: '',
         name: '',
@@ -201,90 +187,8 @@ var app = new Vue({
         type: '人工巡检',
         inspect_val: '',
         detail: '',
+        isEdit: true
       });
-
-
-
-
-      // this.countNum ++;
-      // console.log(this.countNum);
-      // if (this.countNum % 2 == 0) {
-      //   this.addDialogTable.push({
-      //     name: '1',
-      //     describe: '2',
-      //     reference: '3',
-      //     unit: '4',
-      //     inspect_val: '5',
-      //     type: '人工巡检',
-      //     opera: null,
-      //   });
-      // } else {
-      //   let cell_tr = document.createElement('tr');
-      //   cell_tr.setAttribute('class', 'el-table__row');
-      //   for (let i = 0; i < 7; i++) {
-      //     let td = document.createElement('td');
-      //     let input = document.createElement('input');
-      //     let input1 = document.createElement('input');
-      //     let br = document.createElement('br');
-      //     let br1 = document.createElement('br');
-      //     let text = document.createTextNode("~");
-      //     let text1 = document.createTextNode("");
-      //     let div1 = document.createElement("div");
-      //     let div2 = document.createElement("div");
-      //     let span1 = document.createElement('span');
-      //     td.setAttribute('class', 'com-txt-overflow');
-      //     td.style.padding = '2px 4px';
-      //     input.setAttribute('style', 'padding:4px;');
-      //     input1.setAttribute('style', 'padding:4px; margin-top: 5px;');
-      //     switch (i) {
-      //       case 0:
-      //         td.appendChild(input);
-      //         input.placeholder = '巡检项名称';
-      //         break;
-      //       case 1:
-      //         td.appendChild(input);
-      //         input.placeholder = '巡检项描述';
-      //         break;
-      //       case 2:
-      //         td.appendChild(input);
-      //         input.placeholder = '最小值';
-      //         td.appendChild(br);
-      //         td.appendChild(text);
-      //         td.appendChild(br1);
-      //         input1.placeholder = '最大值';
-      //         td.appendChild(input1);
-      //         break;
-      //       case 3:
-      //         td.appendChild(input);
-      //         input.placeholder = '单位';
-      //         break;
-      //       case 4:
-      //         td.appendChild(text1);
-      //         break;
-      //       case 5:
-      //         div1.innerText = '人工巡检';
-      //         div1.className = 'cell';
-      //         td.style.padding = '6px 0';
-      //         td.appendChild(div1);
-      //         break;
-      //       case 6:
-      //         div2.className = 'cell';
-      //         span1.innerHTML = '<span class="el-icon-close com-hover"></span>'
-      //         div2.appendChild(span1);
-      //         span1.setAttribute('onClick', 'deleteNode()');
-      //         // span1.onClick = (evt) => {
-      //         //   console.log('eee', evt)
-      //         //   this.$refs.addTable.bodyWrapper.firstChild.lastChild.removeChild(cell_tr);
-      //         // };
-      //         td.style.padding = '6px 0';
-      //         td.appendChild(div2);
-      //         break;
-      //     }
-      //     cell_tr.appendChild(td);
-      //   }
-      //   this.$refs.addTable.bodyWrapper.firstChild.lastChild.appendChild(cell_tr)
-        // console.log(this.$refs.addTable, this.$refs.addTable.bodyWrapper.firstChild.lastChild);
-      // }
     },
     deleteNode() {
       console.log('eee')
